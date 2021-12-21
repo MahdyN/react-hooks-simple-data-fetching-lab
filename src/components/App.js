20733752f44eac2f5887e709574fb357cc874fb5
Mahdy Noureddine <mahdy.a.noureddine@gmail.com>
2021-12-21 12:51:11 -0500

    commit tree 1e5f73620714b3e946c2024f6d2ecb100b50ef97

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,31 @@
-// create your App component here
+import React, {useState, useEffect} from "react";
+
+function App() {
+    const [image, setImage] = useState("")
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((dogImage) => {
+            setImage(dogImage.message)
+        });
+    },[])
+
+    return(
+
+        <div>
+
+        {image ? 
+        <img src = {image} alt = "A Random Dog" />:
+        <p>Loading...</p>
+        }
+
+
+        </div>      
+    );
+
+
+
+
+}
+
+export default App
